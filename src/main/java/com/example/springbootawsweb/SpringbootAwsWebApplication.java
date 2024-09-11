2789778d278db69bb65845eca1cc24ec7be3bb12
hyunseo <wndlthsk@naver.com>
2024-09-11 16:43:05 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/springbootawsweb/SpringbootAwsWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootawsweb/SpringbootAwsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
