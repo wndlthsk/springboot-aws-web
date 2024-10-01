e93307b86c8557a29e8ac3447ba55580b8a5c97b
hyunseo <wndlthsk@naver.com>
2024-10-01 16:34:57 +0900

    chore: 설정 및 초기 테스트

--- src/main/java/com/example/springbootawsweb/Application.java
+++ src/main/java/com/example/springbootawsweb/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootAwsWebApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAwsWebApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
